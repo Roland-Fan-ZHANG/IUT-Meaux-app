b4de38dd0e4c3de2f78fff2a55ca6d8f69e7b18c
RakanAD <zhangroland385@gmail.com>
2022-10-21 12:03:41 +0200

    Update Minimap script
    Grow + Reduce function disable and enable arrows

--- Assets/Scripts/Minimap.cs
+++ Assets/Scripts/Minimap.cs
@@ -10,6 +10,17 @@ public class Minimap : MonoBehaviour
     public GameObject grow;
     [Header("Reduire la carte")]
     public GameObject reduce;
+
+    public GameObject[] arrows;
+
+/*    public void Start()
+    {
+        arrows = GameObject.FindGameObjectsWithTag("Arrow");
+        for (int i = 0; i < arrows.Length; i++)
+        {
+            arrows[i].SetActive(false);
+        }
+    }*/
     void LateUpdate()
     {
         transform.rotation = Quaternion.Euler(0f, player.eulerAngles.y * -1f, 0f);
@@ -17,11 +28,24 @@ public class Minimap : MonoBehaviour
 
     public void Grow()
     {
+        arrows = GameObject.FindGameObjectsWithTag("Arrow");
+        for (int i = 0; i < arrows.Length; i++)
+        {
+            arrows[i].SetActive(false);
+        }
+        /*foreach (GameObject ar in arrows)
+        {
+            ar.SetActive(false);                //check Disable.cs, Disable une liste avec une boucle For ou avec Foreach
+        }*/  
         reduce.SetActive(false);
         grow.SetActive(true);
     }
     public void Reduce()
     {
+        for (int i = 0; i < arrows.Length; i++)
+        {
+            arrows[i].SetActive(true);
+        }
         reduce.SetActive(true);
         grow.SetActive(false);
     }
